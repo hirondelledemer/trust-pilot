470d85798be82e14efe07aca3c18329ae7224e5a
meda <meda.siaudvytyte@gmail.com>
2024-05-31 20:27:35 +0300

    initial setup

---
+++ src/app/page.tsx
@@ -0,0 +1,62 @@
+'use client';
+
+import Image from 'next/image';
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+import { createClient } from '@/utils/supabase/client';
+// import { PrismaClient } from '@prisma/client';
+import prisma from '../lib/prisma';
+
+export default function Home() {
+  // const supabase = createClient();
+  // const prisma = new PrismaClient({});
+  const handleSubmit = async () => {
+    // try {
+    // setLoading(true)
+    // const { error } = await prisma..from('Review').upsert({
+    //   title: 'test',
+    //   rating: 1,
+    //   content: 'trolol ol ol ol o l o',
+    //   date: new Date(),
+    // });
+    //   const newUser = await prisma.review.create({
+    //     data: {
+    //       title: 'test',
+    //       rating: 1,
+    //       content: 'trolol ol ol ol o l o',
+    //       date: new Date(),
+    //     },
+    //   });
+    //   console.log('new', newUser);
+    //   // if (error) throw error;
+    //   alert('Profile updated!');
+    // } catch (error) {
+    //   console.log(error);
+    //   alert('Error updating the data!');
+    // } finally {
+    //   // setLoading(false);
+    // }
+
+    try {
+      const body = {
+        title: 'test',
+        rating: 1,
+        content: 'trolol ol ol ol o l o',
+        date: new Date(),
+      };
+      await fetch('/api/reviews', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(body),
+      });
+      // await Router.push('/drafts');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  return (
+    <div>
+      here
+      <button onClick={handleSubmit}>sumbit review</button>
+    </div>
+  );
+}
