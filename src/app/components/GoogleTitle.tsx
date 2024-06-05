e22d76c7d584bc3edbae76afb6084ac6b22863ad
meda <meda.siaudvytyte@gmail.com>
2024-06-05 18:09:08 +0300

    fix styles

--- src/app/components/GoogleTitle.tsx
+++ src/app/components/GoogleTitle.tsx
@@ -1,23 +1,19 @@
 export default function GoogleTitle() {
   return (
-    <div className="font-normal box-border bg-white p-[24px] border-b-[rgb(229,229,221)] border-b border-solid">
-      <div className="font-normal box-border items-center flex justify-start w-[510px] mx-[673.5px] my-0">
-        <span className="font-normal box-border block h-[42px] w-[57px] ml-0 mr-[8px] my-0">
-          <picture className="font-normal box-border items-center flex h-[42px] justify-center w-[57px]">
+    <div className="bg-white p-[24px] border-b-[rgb(229,229,221)] border-b border-solid">
+      <div className="items-center flex justify-center">
+        <span className="block h-[42px] w-[57px] mr-[8px]">
+          <picture className=" items-center flex h-[42px] justify-center w-[57px]">
             <img
               src="https://consumersiteimages.trustpilot.net/business-units/46a7e93b000064000500918b-198x149-1x.jpg"
               alt=""
-              className="font-normal box-border max-h-full max-w-full"
+              className="max-h-full max-w-full"
             />
           </picture>
         </span>
-        <span className="font-normal box-border">
-          <h1 className="text-[rgb(28,28,28)] text-[16px] font-bold box-border m-0">
-            Google
-          </h1>
-          <p className="text-[rgb(105,106,106)] text-[16px] font-normal box-border m-0">
-            www.google.com
-          </p>
+        <span>
+          <h1 className="text-[rgb(28,28,28)] text-[16px] font-bold">Google</h1>
+          <p className="text-[rgb(105,106,106)] text-[16px]">www.google.com</p>
         </span>
       </div>
     </div>
--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,24 +1,25 @@
 export default function Header() {
   return (
-    <header className="bg-[rgb(28,28,28)] relative w-[1905px] z-[90] font-normal box-border">
-      <div
-        data-navigation="true"
-        className="items-center flex h-[72px] max-w-[1360px] font-normal box-border mx-[272.5px] my-0 px-[80px] py-0"
-      >
-        <a
-          href="/"
-          target="_self"
-          data-company-logo-link="true"
-          className="text-black block h-[37px] w-[140px] flex-[0_0_auto] font-normal box-border"
+    <header className="bg-[rgb(28,28,28)] w-screen z-[90] flex justify-center">
+      <div className="">
+        <div
+          data-navigation="true"
+          className="items-center justify-start flex h-[72px] w-screen lg:w-[1360px] px-[80px]"
         >
-          <img
-            alt="Trustpilot reviews"
-            width={140}
-            src="https://cdn.trustpilot.net/brand-assets/4.3.0/logo-white.svg"
-            className="font-normal box-border"
-          />
-        </a>
-        <div className="items-center flex flex-1 min-w-0 font-normal box-border pl-[32px] pr-[16px] py-0" />
+          <a
+            href="/"
+            target="_self"
+            data-company-logo-link="true"
+            className="text-black block h-[37px] w-[140px] flex-[0_0_auto]"
+          >
+            <img
+              alt="Trustpilot reviews"
+              width={140}
+              src="https://cdn.trustpilot.net/brand-assets/4.3.0/logo-white.svg"
+            />
+          </a>
+          <div className="items-center flex flex-1 min-w-0 pl-[32px] pr-[16px]" />
+        </div>
       </div>
     </header>
   );
